7b24ef7839f5a80e1d369f1cb63b7980d5a0841e
Aryadev Chavali <aryadev@aryadevchavali.com>
2020-07-20 23:10:59 +0100

    +script options to allow switch to C++
    Calls 'sed' on the CMakeLists.txt file, switching all C options to C++.
    Then it writes a new main file and calls CMake

--- start.sh
+++ start.sh
@@ -2,5 +2,23 @@
 
 mkdir dist includes;
 echo "" > README.org;
+rm -rfv start.sh LICENSE;
+
+# Now for the hard part
+if [[ $1 == "cpp" ]]
+then
+    sed -i 's/\*\.c/*.cpp/g;s/\*\.h/*.hpp/g;s/\${THIS} C/${THIS} CXX/g;' CMakeLists.txt;
+    rm src/main.c;
+    echo "#include<iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+  cout << \"Hello, world!\" << endl;
+  return 0;
+}" > src/main.cpp
+fi
+
 cd dist; cmake ..; cd ..;
-rm -rfv start.sh LICENSE .git;
