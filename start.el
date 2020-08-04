5c73162e28e323a1e620ab21d9121463b8b46eb5
Aryadev Chavali <aryadev@aryadevchavali.com>
2020-08-04 20:52:31 +0100

    ~rely on the scripts I've written: use start.sh in start.el
    Instead of using the (while good, excessive) abilities of elisp, just
    use the script I've already written to provide a solution

--- start.el
+++ start.el
@@ -5,39 +5,8 @@
 ;;; Code:
 
 (let ((dx:cc-type (completing-read "Enter type of project: " '(c cpp) nil t))
-      (dx:cc-project-name (completing-read "Enter name of project: " nil)))
-  (ignore-errors
-    (make-directory "dist")
-    (make-directory "includes")
-    (make-empty-file "README.org")
-    (delete-file "start.sh")
-    (delete-file "LICENSE")
-    (delete-file "start.el"))
-  
-  (when (string= dx:cc-type "cpp")
-    (delete-file "src/main.c")
-    (with-temp-buffer (find-file "CMakeLists.txt")
-                      (replace-string "main" dx:cc-project-name)
-                      (replace-string ".c" ".cpp")
-                      (replace-string ".h" ".hpp")
-                      (replace-string "${THIS} C" "${THIS} CXX")
-                      (save-buffer))
-    (with-temp-buffer (find-file "src/main.cpp")
-                      (insert "#include<iostream>
-
-using std::cout;
-using std::endl;
-
-int main()
-{
-  cout << \"Hello, world!\" << endl;
-  return 0;
-}
-")
-                      (save-buffer)))
-  (with-temp-buffer (find-file "Makefile")
-                    (replace-string "main" dx:cc-project-name)
-                    (save-buffer)))
+       (dx:cc-project-name (completing-read "Enter name of project: " nil)))
+  (shell-command ("sh start.sh %s %s" dx:cc-type dx:cc-project-name)))
 
 (provide 'start)
 ;;; start.el ends here
