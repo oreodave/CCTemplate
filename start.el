827ed1ee50eebe13a09c92e8dbdc665d126906ea
Aryadev Chavali <aryadev@aryadevchavali.com>
2020-08-04 20:26:02 +0100

    ~added ignore errors call to directory and file manipulation

--- start.el
+++ start.el
@@ -6,20 +6,22 @@
 
 (let ((dx:cc-type (completing-read "Enter type of project: " '(c cpp) nil t))
       (dx:cc-project-name (completing-read "Enter name of project: " nil nil t)))
-  (make-directory "dist")
-  (make-directory "includes")
-  (make-empty-file "README.org")
-  (delete-file "start.sh")
-  (delete-file "LICENSE")
-  (delete-file "start.el")
-  (when (= dx:cc-type cpp)
-    (with-current-buffer (find-file "CMakeLists.txt")
+  (ignore-errors
+    (make-directory "dist")
+    (make-directory "includes")
+    (make-empty-file "README.org")
+    (delete-file "start.sh")
+    (delete-file "LICENSE")
+    (delete-file "start.el"))
+  (when (string= dx:cc-type "cpp")
+    (delete-file "src/main.c")
+    (with-temp-buffer (find-file "CMakeLists.txt")
       (replace-string "main" dx:cc-project-name)
       (replace-string ".c" ".cpp")
       (replace-string ".h" ".hpp")
-      (replace-string "${THIS} C" "${THIS} CXX"))
-    (delete-file "src/main.c")
-    (with-current-buffer (find-file "src/main.cpp")
+      (replace-string "${THIS} C" "${THIS} CXX")
+      (save-buffer))
+    (with-temp-buffer (find-file "src/main.cpp")
       (insert "#include<iostream>
 
 using std::cout;
@@ -30,8 +32,9 @@ int main()
   cout << \"Hello, world!\" << endl;
   return 0;
 }
-")))
-  (with-current-buffer (find-file "Makefile")
+")
+      (save-buffer)))
+  (with-temp-buffer (find-file "Makefile")
     (replace-string "main" dx:cc-project-name)))
 
 (provide 'start)
