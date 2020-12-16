43d3c2c3aa25cef8370dbdef066c0ee000b3d691
Aryadev Chavali <aryadev@aryadevchavali.com>
2020-12-16 07:02:36 +0000

    (start.el)+format command to make shell-command

--- start.el
+++ start.el
@@ -6,7 +6,7 @@
 
 (let ((dx:cc-type (completing-read "Enter type of project: " '(c cpp) nil t))
        (dx:cc-project-name (completing-read "Enter name of project: " nil)))
-  (shell-command ("sh start.sh %s %s" dx:cc-type dx:cc-project-name)))
+  (shell-command (format "sh start.sh %s %s" dx:cc-type dx:cc-project-name)))
 
 (provide 'start)
 ;;; start.el ends here
